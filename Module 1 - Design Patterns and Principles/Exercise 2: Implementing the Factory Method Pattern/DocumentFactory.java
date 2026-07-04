2be7cbc1785ba193738cd62a4479628585c883aa
Anlin Abishek J <165697246+Anlin-Abishek@users.noreply.github.com>
2026-07-04 19:05:17 +0530

    Implement Factory Method for document creation

--- Module 1 - Design Patterns and Principles/Exercise 2: Implementing the Factory Method Pattern/DocumentFactory.java
+++ Module 1 - Design Patterns and Principles/Exercise 2: Implementing the Factory Method Pattern/DocumentFactory.java
@@ -1,3 +1,24 @@
 public abstract class DocumentFactory {
     public abstract Document createDocument();
 }
+
+public class WordDocumentFactory extends DocumentFactory {
+    @Override
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+public class PdfDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening PDF Document...");
+    }
+}
+
+public class ExcelDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening Excel Document...");
+    }
+}
