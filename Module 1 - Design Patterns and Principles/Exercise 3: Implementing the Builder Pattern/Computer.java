ae8e561837d41cb48fe878182ba6ca66b5dcb232
Anlin Abishek J <165697246+Anlin-Abishek@users.noreply.github.com>
2026-07-04 19:09:12 +0530

    Create Computer.java

---
+++ Module 1 - Design Patterns and Principles/Exercise 3: Implementing the Builder Pattern/Computer.java
@@ -0,0 +1,55 @@
+public class Computer {
+    private final String hdd;
+    private final String ram;
+    private final boolean isGraphicsCardEnabled;
+    private final boolean isBluetoothEnabled;
+
+    public String getHdd() {
+        return hdd;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
+    }
+
+    public boolean isBluetoothEnabled() {
+        return isBluetoothEnabled;
+    }
+
+    private Computer(ComputerBuilder builder) {
+        this.hdd = builder.hdd;
+        this.ram = builder.ram;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    }
+
+    public static class ComputerBuilder {
+        private final String hdd;
+        private final String ram;
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public ComputerBuilder(String hdd, String ram) {
+            this.hdd = hdd;
+            this.ram = ram;
+        }
+
+        public ComputerBuilder setGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
+            this.isGraphicsCardEnabled = isGraphicsCardEnabled;
+            return this;
+        }
+
+        public ComputerBuilder setBluetoothEnabled(boolean isBluetoothEnabled) {
+            this.isBluetoothEnabled = isBluetoothEnabled;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
